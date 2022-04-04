12ab287936ed046aacc4ac71d7a723d45de0a802
Jennifer Lee <jenlee.tech@gmail.com>
2022-04-03 22:06:36 -0400

    moved the dummy users array to the UserFinder component

--- src/components/UserFinder.js
+++ src/components/UserFinder.js
@@ -1,6 +1,12 @@
 import { Fragment, useState, useEffect } from "react";
-
+import classes from "./UserFinder.module.css";
 import Users from "./Users";
+
+const DUMMY_USERS = [
+  { id: "u1", name: "Max" },
+  { id: "u2", name: "Manuel" },
+  { id: "u3", name: "Julie" },
+];
 
 const UserFinder = () => {
   const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS);
