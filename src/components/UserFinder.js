c896eabca610234f5b79366c3f754bf5389bd43b
Jennifer Lee <jenlee.tech@gmail.com>
2022-04-17 22:06:43 -0400

    extended component class for UserFinder and created state

--- src/components/UserFinder.js
+++ src/components/UserFinder.js
@@ -1,6 +1,15 @@
-import { Fragment, useState, useEffect } from "react";
+import { Fragment, useState, useEffect, Component } from "react";
 import classes from "./UserFinder.module.css";
 import Users from "./Users";
+
+class UserFinder extends Component {
+  constructor() {
+    this.state = {
+      filterUsers: DUMMY_USERS,
+      searchTerm: "",
+    };
+  }
+}
 
 const DUMMY_USERS = [
   { id: "u1", name: "Max" },
